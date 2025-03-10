ef05c48a79cf150deacc07699cee1d6a7f642810
TheDenizA <deniz.akintetik@gmail.com>
2025-03-10 15:48:40 +1000

    added tests

---
+++ CodingTextTLC.Tests/Repositories/PurchaseRepoTests.cs
@@ -0,0 +1,105 @@
+﻿using CodingTestTLC.Exceptions;
+using CodingTestTLC.Models;
+using CodingTestTLC.Repositories;
+
+namespace CodingTextTLC.Tests.Services;
+
+public class PurchaseRepoTests
+{
+    [Test]
+    public async Task ShouldCreatePurchaseRequests()
+    {
+        IDatabase mockDatabase = new Database();
+        IPurchaseRepo repo = new PurchaseRepo(mockDatabase);
+
+        var inputModel1 = new LotteryRequestModel("custID-1", "drawID-1", 1);
+        var inputModel2 = new LotteryRequestModel("custID-2", "drawID-2", 2);
+        var inputModel3 = new LotteryRequestModel("custID-3", "drawID-3", 3);
+
+        await repo.CreateAsync(inputModel1);
+        await repo.CreateAsync(inputModel2);
+        await repo.CreateAsync(inputModel3);
+
+        var dbPurchaseRequests = await mockDatabase.GetPurchaseRequestsAsync();
+
+        Assert.That(dbPurchaseRequests.Count, Is.EqualTo(3));
+
+        Assert.That(dbPurchaseRequests[0].CustomerId, Is.EqualTo("custID-1"));
+        Assert.That(dbPurchaseRequests[0].DrawId, Is.EqualTo("drawID-1"));
+        Assert.That(dbPurchaseRequests[0].NumberOfTickets, Is.EqualTo(1));
+        Assert.That(dbPurchaseRequests[0].UniquePurchaseId, Is.EqualTo(1));
+        Assert.That(dbPurchaseRequests[0].PurchaseTotal, Is.EqualTo(null));
+
+        Assert.That(dbPurchaseRequests[1].CustomerId, Is.EqualTo("custID-2"));
+        Assert.That(dbPurchaseRequests[1].DrawId, Is.EqualTo("drawID-2"));
+        Assert.That(dbPurchaseRequests[1].NumberOfTickets, Is.EqualTo(2));
+        Assert.That(dbPurchaseRequests[1].UniquePurchaseId, Is.EqualTo(2));
+        Assert.That(dbPurchaseRequests[1].PurchaseTotal, Is.EqualTo(null));
+
+        Assert.That(dbPurchaseRequests[2].CustomerId, Is.EqualTo("custID-3"));
+        Assert.That(dbPurchaseRequests[2].DrawId, Is.EqualTo("drawID-3"));
+        Assert.That(dbPurchaseRequests[2].NumberOfTickets, Is.EqualTo(3));
+        Assert.That(dbPurchaseRequests[2].UniquePurchaseId, Is.EqualTo(3));
+        Assert.That(dbPurchaseRequests[2].PurchaseTotal, Is.EqualTo(null));
+    }
+
+    [Test]
+    public async Task ShouldUpdatePurchaseRequest()
+    {
+        IDatabase mockDatabase = new Database();
+        IPurchaseRepo repo = new PurchaseRepo(mockDatabase);
+
+        // Add Test Data
+        var inputModel1 = new LotteryRequestModel("custID-1", "drawID-1", 1);
+        var inputModel2 = new LotteryRequestModel("custID-2", "drawID-2", 2);
+        var inputModel3 = new LotteryRequestModel("custID-3", "drawID-3", 3);
+
+        await repo.CreateAsync(inputModel1);
+        await repo.CreateAsync(inputModel2);
+        await repo.CreateAsync(inputModel3);
+
+        inputModel2.ConfirmPurchase(45.60m);
+        await repo.UpdateAsync(inputModel2);
+
+        var dbPurchaseRequests = await mockDatabase.GetPurchaseRequestsAsync();
+
+        Assert.That(dbPurchaseRequests.Count, Is.EqualTo(3));
+
+        // Ensure Not updated
+        Assert.That(dbPurchaseRequests[0].CustomerId, Is.EqualTo("custID-1"));
+        Assert.That(dbPurchaseRequests[0].DrawId, Is.EqualTo("drawID-1"));
+        Assert.That(dbPurchaseRequests[0].NumberOfTickets, Is.EqualTo(1));
+        Assert.That(dbPurchaseRequests[0].UniquePurchaseId, Is.EqualTo(1));
+        Assert.That(dbPurchaseRequests[0].PurchaseTotal, Is.EqualTo(null));
+
+        // This should have only purchase total updated
+        Assert.That(dbPurchaseRequests[1].CustomerId, Is.EqualTo("custID-2"));
+        Assert.That(dbPurchaseRequests[1].DrawId, Is.EqualTo("drawID-2"));
+        Assert.That(dbPurchaseRequests[1].NumberOfTickets, Is.EqualTo(2));
+        Assert.That(dbPurchaseRequests[1].UniquePurchaseId, Is.EqualTo(2));
+        Assert.That(dbPurchaseRequests[1].PurchaseTotal, Is.EqualTo(45.60m));
+
+        // Ensure Not updated
+        Assert.That(dbPurchaseRequests[2].CustomerId, Is.EqualTo("custID-3"));
+        Assert.That(dbPurchaseRequests[2].DrawId, Is.EqualTo("drawID-3"));
+        Assert.That(dbPurchaseRequests[2].NumberOfTickets, Is.EqualTo(3));
+        Assert.That(dbPurchaseRequests[2].UniquePurchaseId, Is.EqualTo(3));
+        Assert.That(dbPurchaseRequests[2].PurchaseTotal, Is.EqualTo(null));
+    }
+
+    [Test]
+    public async Task ShouldThrowWhenUpdatePurchaseRequestFailsToFindData()
+    {
+        IDatabase mockDatabase = new Database();
+        IPurchaseRepo repo = new PurchaseRepo(mockDatabase);
+
+        var inputModel1 = new LotteryRequestModel("custID-1", "drawID-1", 1);
+        inputModel1.ConfirmPurchase(45.60m);
+
+        Assert.ThrowsAsync<NotFoundLotteryRequestException>(() => repo.UpdateAsync(inputModel1));
+
+        var dbPurchaseRequests = await mockDatabase.GetPurchaseRequestsAsync();
+
+        Assert.That(dbPurchaseRequests.Count, Is.EqualTo(0));
+    }
+}
---
+++ CodingTextTLC.Tests/Services/LotteryServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using CodingTestTLC.Models;
+using CodingTestTLC.Partners;
+using CodingTestTLC.Repositories;
+using CodingTestTLC.Services;
+using Moq;
+
+namespace CodingTextTLC.Tests.Services;
+
+public class LotteryServiceTests
+{
+    private Mock<IPurchaseRepo> _mockPurchaseRepo;
+    private Mock<IThirdPartyService> _mockThirdPartyService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockPurchaseRepo = new Mock<IPurchaseRepo>();
+        _mockThirdPartyService = new Mock<IThirdPartyService>();
+    }
+
+    [Test]
+    public async Task ShouldPurchaseLotteryTicket()
+    {
+        var createModel = new LotteryRequestModel("custID-1", "drawID-1", 5);
+        createModel.AddUniquePurchaseId(7);
+        _mockPurchaseRepo.Setup(x => x.CreateAsync(createModel)).Returns(Task.CompletedTask);
+
+        _mockThirdPartyService.Setup(x => x.RequestPurchaseAsync(createModel)).Returns(Task.FromResult(new LotteryResponseModel(100m)));
+
+        var updateModel = new LotteryRequestModel("custID-1", "drawID-1", 5);
+        updateModel.AddUniquePurchaseId(7);
+        updateModel.ConfirmPurchase(100m);
+        _mockPurchaseRepo.Setup(x => x.UpdateAsync(updateModel)).Returns(Task.CompletedTask);
+
+        var lotteryService = new LotteryService(_mockPurchaseRepo.Object, _mockThirdPartyService.Object);
+
+        var purchasedTicket = await lotteryService.PurchaseLotteryTicketAsync(createModel);
+
+        Assert.That(purchasedTicket.CustomerId, Is.EqualTo("custID-1"));
+        Assert.That(purchasedTicket.DrawId, Is.EqualTo("drawID-1"));
+        Assert.That(purchasedTicket.NumberOfTickets, Is.EqualTo(5));
+        Assert.That(purchasedTicket.UniquePurchaseId, Is.EqualTo(7));
+        Assert.That(purchasedTicket.PurchaseTotal, Is.EqualTo(100m));
+    }
+}
